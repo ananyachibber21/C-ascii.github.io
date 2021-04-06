e76d21d6971cbe44a19e497e00135c3365c0e6ec
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-04-06 13:20:06 +0530

    Create ascii value.cpp

---
+++ ascii value.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+	char a;
+
+    printf("Enter a character: ");
+
+    scanf("%c",&a);
+
+    printf("The ascii value of %c is %d",a,a);
+	
+	
+}
